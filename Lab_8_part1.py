cd4b263e4330942f82c9538d77b6298cb42fbe94
ShaneRandell <77622184+ShaneRandell@users.noreply.github.com>
2021-02-21 16:23:16 -0400

    Add files via upload

--- Lab_8_part1.py
+++ Lab_8_part1.py
@@ -9,12 +9,20 @@ minPW = 1/1000
 def slider_read(slider_value):
     textbox.value = slider_value
     print(slider_value)
-    servo1 = AngularServo(17, min_pulse_width = minPW, max_pulse_width = maxPW, initial_angle = 0, min_angle = -90, max_angle = 90)
+    servo1 = AngularServo(20, min_pulse_width = minPW, max_pulse_width = maxPW, initial_angle = 0, min_angle = -90, max_angle = 90)
     servo1.angle = int(slider_value)    
-    time.sleep(0.5)    
+    time.sleep(0.5)
+    
+def slider2_read(slider_value):
+    textbox.value = slider_value
+    print(slider_value)
+    servo2 = AngularServo(21, min_pulse_width = minPW, max_pulse_width = maxPW, initial_angle = 0, min_angle = -90, max_angle = 90)
+    servo2.angle = int(slider_value)    
+    time.sleep(0.5)     
 
 app = App()
 slider1 = Slider(app, start=-90, end=90, width="fill",command = slider_read)
+slider2 = Slider(app, start=-90, end=90, width="fill",command = slider2_read)
 textbox = TextBox(app)
 
 print(textbox.value)
